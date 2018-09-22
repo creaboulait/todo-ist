6d404c2eae2d1373186b92a454b2c0332c4309aa
creaboulait <norshida.razali@gmail.com>
2018-09-22 16:56:17 +0800

    Complete Edit and Destroy item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,17 @@ class ItemsController < ApplicationController
 	end
 
 	def show
-		
+	end
+
+	def edit
+	end
+
+	def update
+		if @item.update(item_params)
+			redirect_to item_path(@item)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
